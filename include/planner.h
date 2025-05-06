c8e745259b624beca02f51e29a7a9f28fea361e9
dondaha <1656982710@qq.com>
2025-05-07 03:17:24 +0800

    初始化了planner

--- include/planner.h
+++ include/planner.h
@@ -1,6 +1,13 @@
 #ifndef __PLANNER_H__
 #define __PLANNER_H__
 
-
+class Planner
+{
+public:
+    Planner(double L1, double L2, double L3, double x, double y, double r);
+    ~Planner();
+    void planTrajectory();
+    void pointsSampler();
+};
 
 #endif
